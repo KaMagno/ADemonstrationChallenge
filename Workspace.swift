250ced16b5e9607541bc22b75b650a2ce2a7d26a
Kaique Magno <kaique.magno@hotmail.com>
2023-02-14 15:17:00 -0300

    Add AllTests Target in Workspace

--- Workspace.swift
+++ Workspace.swift
@@ -1,5 +1,16 @@
 import ProjectDescription
 
+let allTestsTargets = Scheme(name: "AllTests",
+                             shared: true,
+                             hidden: false,
+                             testAction: .targets([
+                                TestableTarget(target: .project(path: "./Features/Exchanges",
+                                                                target: "ExchangesTests")),
+                                TestableTarget(target: .project(path: "./Tools/MBNetwork",
+                                                                target: "MBNetworkTests"))
+                             ])
+)
+
 let workspace = Workspace(name: "MercadoBitcoinChallange",
                           projects: [
                             "App",
@@ -7,6 +18,9 @@ let workspace = Workspace(name: "MercadoBitcoinChallange",
                             "Tools/MBNetwork",
                             "Tools/Commons",
                             "Tools/MBUI",
+                          ],
+                          schemes: [
+                            allTestsTargets
                           ],
                           additionalFiles: [
                             .glob(pattern: "./README.MD")
